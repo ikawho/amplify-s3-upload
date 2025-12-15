98d5b1627300fadae2404bc19c0288a515a1e04a
ikawho <andrii.ikaliuk@gmail.com>
2025-12-15 13:24:12 +0100

    Clean frontend setup (resolve conflicts, frontend-only Amplify)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
-import App from "./App.tsx";
-import "./index.css";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from "@aws-amplify/ui-react";
+import App from "./App";
+import "./index.css";
+import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure(outputs);
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "amplifyAuthUserPool4BA7F805-1vRp01gYI5TJ",
+      userPoolClientId: "5h8l13cfskuikqveub99l6gj5",
+      loginWith: {
+        email: true,
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -16,3 +25,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </Authenticator>
   </React.StrictMode>
 );
+
